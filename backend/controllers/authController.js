975e4f78c66ebc26be63d5196d38471d5793bfce
LegolasArkantos <unasaamir101@gmail.com>
2023-11-03 17:44:54 +0500

    refresh

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -75,8 +75,27 @@ const login = async (req, res) => {
         res.status(500).json({ message: 'Internal Server Error' });
     }
 };
+//refresh token when access token expire
+const refreshTokens = (req, res) => {
+    const refreshToken = req.body.refreshToken;
+
+    if (!refreshToken) {
+        return res.status(401).json({ message: 'Refresh token not provided' });
+    }
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: 'Invalid refresh token' });
+        }
+
+        const accessToken = jwt.sign({ userId: user.userId }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+
+        res.json({ accessToken });
+    });
+};
 
 module.exports = {
     signup,
     login,
+    refreshTokens,
 };
